25aee749a8d2de9db7b8fd2510dc32b3a331289a
skritch <sam.kritch@gmail.com>
2022-11-28 20:39:57 -0500

    Split up overconcentration notebook by dimension

--- dbt/models/analysis/uhf_otp_analysis.sql
+++ dbt/models/analysis/uhf_otp_analysis.sql
@@ -2,6 +2,7 @@ select
     uhf_code,
     uhfs.borough_name,
     uhfs.is_harlem,
+    od.overdose_deaths AS overdose_deaths_2020,
     sum(z.population_total) AS population_2020_estimate,
     sum(total_admissions_1_2017) as total_admissions_1_2017,
     sum(total_admissions_3_2017) as total_admissions_3_2017,
@@ -15,11 +16,10 @@ select
     sum(avg_daily_enrollment_2019) AS avg_daily_enrollment_2019,
     sum(patient_admissions_3_2017) AS patient_admissions_3_2017,
     sum(patient_admissions_3_2019) AS patient_admissions_3_2019,
-    sum(opioid_burden_2019) AS opioid_burden_2019,
-    sum(od.overdose_deaths) AS overdose_deaths_2020
+    sum(opioid_burden_2019) AS opioid_burden_2019
 from {{ ref('zip_code_otp_analysis') }}
     left join {{ ref('zip_codes') }} AS z using (zip_code)
     left join {{ ref('uhfs') }} AS uhfs using (uhf_code)
     left join {{ ref('uhf_overdoses_2020') }} as od using (uhf_code)
 where uhf_code is not null
-group by 1, 2, 3
+group by 1, 2, 3, 4
